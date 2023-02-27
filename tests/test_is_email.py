7a790b1a85968e7a4e1addf1f20235896f5600a5
Ramon <ramon.guimera@skyscanner.net>
2023-02-27 18:20:10 +0100

    fixing tests and adding more cases

--- tests/test_is_email.py
+++ tests/test_is_email.py
@@ -19,7 +19,14 @@ def test_result_as_str():
         ),
         ("test@test", 1, "test.txt", None),
         ("@test", 1, "test.txt", None),
-        ("bad@test.s", 1, "test.txt", None),
+        ("bad@test.s", 1, "test.txt", Result(is_email=True, email="bad@test.s", line_number=1, file_name="test.txt")),
+        ("bad@test.ss", 1, "test.txt", Result(is_email=True, email="bad@test.ss", line_number=1, file_name="test.txt")),
+        (
+            "secure.secure00000@test.ss",
+            1,
+            "test.txt",
+            Result(is_email=True, email="secure.secure00000@test.ss", line_number=1, file_name="test.txt"),
+        ),
     ],
 )
 def test_is_email(line, line_number, file_name, expected):
