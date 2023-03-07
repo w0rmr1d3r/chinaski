b643c785ca48eed4d08a6f172b7770276e2c2af6
Ramon <ramon.guimera@skyscanner.net>
2023-03-07 12:27:56 +0100

    handle unicode error

--- tests/test_read_lines.py
+++ tests/test_read_lines.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 
@@ -34,3 +35,10 @@ def test_read_lines(file, expected):
 
 def test_read_lines_returns_empty_list_when_file_does_not_exist():
     assert read_lines_from_file(filename="file does not exist") == []
+
+
+def test_read_lines_returns_empty_list_when_raising_unicode_error():
+    with patch("builtins.open") as mock_file:
+        mock_file.side_effect = UnicodeDecodeError("funnycodec", b"\x00\x00", 1, 2, "This is just a fake reason!")
+        assert read_lines_from_file(filename="unicode_file") == []
+    mock_file.assert_called_with("unicode_file", "r")
